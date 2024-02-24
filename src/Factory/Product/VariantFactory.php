19db2ce8fdbcb68f0973503efefed95c2fce6cc0
muggystyle24 <muggystyle24@gmail.com>
2024-02-24 11:46:58 +0800

    FIX Channel Pricing Interface

--- src/Factory/Product/VariantFactory.php
+++ src/Factory/Product/VariantFactory.php
@@ -8,11 +8,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Nextstore\SyliusDropshippingCorePlugin\Model\ProductVariantInterface as ModelProductVariantInterface;
 use Nextstore\SyliusOtcommercePlugin\Service\OtResponse;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
-use Sylius\Component\Core\Model\ChannelPricing;
+use Sylius\Component\Core\Model\ChannelPricingInterface;
 use Sylius\Component\Core\Model\ProductVariant;
 use Sylius\Component\Product\Factory\ProductVariantFactoryInterface;
 use Sylius\Component\Product\Model\ProductInterface as ModelProductInterface;
 use Sylius\Component\Product\Model\ProductVariantInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 
 class VariantFactory implements ProductVariantFactoryInterface
 {
@@ -21,6 +22,7 @@ class VariantFactory implements ProductVariantFactoryInterface
         private EntityManagerInterface $entityManager,
         private ChannelContextInterface $channelContext,
         private OtResponse $otResponse,
+        private FactoryInterface $channelPricingFactory,
     ) {
     }
 
@@ -37,7 +39,12 @@ class VariantFactory implements ProductVariantFactoryInterface
     public function createVariantFromOt($itemInfo, $params, $product)
     {
         $configuredItem = $this->otResponse->findConfiguredItem($itemInfo['ConfiguredItems'], $params['configuredItemId']);
-        $attributeInfo = $this->otResponse->findAttributeInfo($itemInfo['Attributes'], $configuredItem);
+        if ($configuredItem === null) {
+            $configuredItem = $itemInfo;
+            $attributeInfo = ['value' => $itemInfo['Title'], 'imageUrl' => $itemInfo['MainPictureUrl']];
+        } else {
+            $attributeInfo = $this->otResponse->findAttributeInfo($itemInfo['Attributes'], $configuredItem);
+        }
         $promotionPrice = $this->otResponse->findMinPromotionPrice($itemInfo, $configuredItem);
 
         /** @var ModelProductVariantInterface $variant */
@@ -60,7 +67,12 @@ class VariantFactory implements ProductVariantFactoryInterface
     public function updateVariantFromOt($itemInfo, $params, ModelProductVariantInterface $variant)
     {
         $configuredItem = $this->otResponse->findConfiguredItem($itemInfo['ConfiguredItems'], $params['configuredItemId']);
-        $attributeInfo = $this->otResponse->findAttributeInfo($itemInfo['Attributes'], $configuredItem);
+        if ($configuredItem === null) {
+            $configuredItem = $itemInfo;
+            $attributeInfo = ['value' => $itemInfo['Title'], 'imageUrl' => $itemInfo['MainPictureUrl']];
+        } else {
+            $attributeInfo = $this->otResponse->findAttributeInfo($itemInfo['Attributes'], $configuredItem);
+        }
         $promotionPrice = $this->otResponse->findMinPromotionPrice($itemInfo, $configuredItem);
 
         $variant->setName($attributeInfo['value']);
@@ -74,7 +86,7 @@ class VariantFactory implements ProductVariantFactoryInterface
         return $variant;
     }
 
-    private function createChannelPricing($configuredItem, $promotionPrice): ChannelPricing
+    private function createChannelPricing($configuredItem, $promotionPrice): ChannelPricingInterface
     {
         $oneItemPriceWithoutDelivery = $configuredItem['Price']['ConvertedPriceList']['Internal']['Price'];
 
@@ -82,7 +94,7 @@ class VariantFactory implements ProductVariantFactoryInterface
         $price = $promotionPrice;        
 
         $channel = $this->channelContext->getChannel();
-        $cp = new ChannelPricing();
+        $cp = $this->channelPricingFactory->createNew();
         $cp->setChannelCode($channel->getCode());
         $cp->setMinimumPrice(0);
         $cp->setOriginalPrice($originalPrice);      // Үндсэн үнэ
@@ -93,7 +105,7 @@ class VariantFactory implements ProductVariantFactoryInterface
         return $cp;
     }
 
-    private function updateChannelPricing($configuredItem, $promotionPrice, ChannelPricing $cp): ChannelPricing
+    private function updateChannelPricing($configuredItem, $promotionPrice, ChannelPricingInterface $cp): ChannelPricingInterface
     {
         $oneItemPriceWithoutDelivery = $configuredItem['Price']['ConvertedPriceList']['Internal']['Price'];
 
